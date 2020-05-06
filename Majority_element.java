e8a3d9a3fd010888346a9b58ca5889140dc86ce0
Murali Krishnan V <43649148+Murali2742@users.noreply.github.com>
2020-05-06 18:53:43 +0530

    Update Majority_element.java

--- Majority_element.java
+++ Majority_element.java
@@ -5,8 +5,8 @@ Input: [3,2,3]
 Output: 3
 Example 2:
 Input: [2,2,1,1,1,2,2]
-Output: 2 *
-/
+Output: 2
+*/
 class Solution {
     public int majorityElement(int[] nums) {
         if(nums.length==1)
